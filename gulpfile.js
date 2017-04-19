1935ab70b2183943260c02ff172c9e64997981ab
tengfeis <tengfeis@jumei.com>
2017-04-19 18:51:33 +0800

    upload

--- gulpfile.js
+++ gulpfile.js
@@ -8,27 +8,25 @@ var dir = './dest/v' + pack.version;
 var path = require('path');
 var map = {};
 var options = {
-	'Connection':'keep-alive',
 	'hostname': 'onlyqing.cn',
 	'port':3000,
 	'path': '/upload',
 	'method': 'POST'     
 }
-
 var readMap = function ( dir ) {
 	let files = fs.readdirSync( dir);
 	for(var i = 0; i < files.length; i++) {
 		let filePath = dir + '/' + files[i];
-		let dirPath = filePath.replace('./','/');
 		let stats = fs.statSync(filePath);
 		if( stats.isFile() ){
+			let file = filePath.replace('./','/');
 			var req = http.request(options, function(res) {
 				res.setEncoding('utf8');
 				res.on('data', function (chunk) {
-					console.log('body:' + chunk);
+					console.log('\t提示:\t' + chunk);
 				});
 			});
-			req.setHeader('to',dirPath);
+			req.setHeader('to',dir.replace('./','/'));
 			req.setHeader('filename',files[i]);
 
 			var readStream = fs.ReadStream(filePath);
@@ -37,7 +35,7 @@ var readMap = function ( dir ) {
 			readStream.on('close', function() {
 				req.end();
 			});
-			map[dirPath] = confPack.staticSite + dirPath;
+			map[file] = confPack.staticSite + file;
 		}else{
 			readMap(filePath);
 		}
@@ -49,5 +47,5 @@ readMap(dir);
 var data = JSON.stringify( map , null ,'\t' );
 
 fs.writeFile(dir + '.json', data ,'utf-8',function (err,file) {
-	console.log(map);
+	
 });
