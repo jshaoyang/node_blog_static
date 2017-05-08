66e43f680c15720d1be52f3575f4bdb11e022e60
tengfeis <tengfeis@jumei.com>
2017-05-08 15:25:32 +0800

    fix

--- pages/index/main.vue
+++ pages/index/main.vue
@@ -1,31 +1,55 @@
 <template>
-	<transition name="view">
-		<router-view></router-view>
-	</transition>
+	<div class="main" ref="main">
+		<transition name="view" v-on:before-enter="beforeEnter" v-on:after-leave="afterLeave">
+			<router-view></router-view>
+		</transition>
+	</div>
 </template>
 <style lang="sass">
 	@import './common.scss';
 	.main{
 		overflow: hidden;
+		width:100%;
 	}
 	.main_content{
 		padding-bottom: 50px;
+		float:left;
+		width:100%;
 	}
 	.view-enter-active {
-		transition: all .4s ease;
+		transition: all .5s ease;
 	}
 	.view-leave-active {
-		transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+		transition: all .5s ease;
 	}
-	.view-enter{
-		transform: translateX(100%);
-		margin-right:-100%;
+	.view-enter-to{
+		transform: translateX(-100%);
 	}
 	.view-leave-to{
-		transform: translateX(-100%);
+		opacity: 0;
 	}
 </style>
 <script type="text/javascript">
 	export default {
+		data () {
+			return {
+				width : window.innerWidth - 15
+			}
+		},
+		mounted () {
+		},
+		methods:{
+			beforeEnter () {
+				var main = this.$refs.main;
+				main.style.width = this.width * 2 + 'px';
+				main.children[0].style.width = this.width + 'px';
+				main.children[1].style.width = this.width + 'px';
+
+			},
+			afterLeave () {
+				var main = this.$refs.main;
+				main.style.width = this.width + 'px';
+			} 
+		}
 	}
 </script>
